dd79e6753f137581b7ace3dbc50f7b386e6e0c07
KAJUGA Daniels <kajugadaniels@gmail.com>
2025-01-08 17:22:12 +0200

    Add userProfile route

--- account/urls.py
+++ account/urls.py
@@ -8,4 +8,5 @@ app_name = 'auth'
 urlpatterns = [
     path('', userLogin, name="login"),
     path('logout/', userLogout, name='logout'),
+    path('profile/', userProfile, name='userProfile'),
 ]  + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
