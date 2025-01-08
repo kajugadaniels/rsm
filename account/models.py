d823482e5d553cadae38dde141e60d5195638c6e
KAJUGA Daniels <kajugadaniels@gmail.com>
2025-01-08 11:54:24 +0200

    Add user model

--- account/models.py
+++ account/models.py
@@ -1,3 +1,88 @@
+import os
 from django.db import models
+from django.utils import timezone
+from django.utils.text import slugify
+from account.managers import *
+from imagekit.processors import ResizeToFill
+from imagekit.models import ProcessedImageField
+from django.utils.translation import gettext_lazy as _
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 
-# Create your models here.
+def user_image_path(instance, filename):
+    base_filename, file_extension = os.path.splitext(filename)
+    return f'profile_images/user_{slugify(instance.slug)}_{instance.phone_number}{file_extension}'
+
+class User(AbstractBaseUser, PermissionsMixin):
+    ROLE_CHOICES = (
+        ('User', 'User'),
+        ('Client', 'Client'),
+    )
+
+    email = models.EmailField(unique=True)
+    name = models.CharField(max_length=255)
+    phone_number = models.CharField(max_length=15, unique=True)
+    image = ProcessedImageField(
+        upload_to=user_image_path,
+        processors=[ResizeToFill(720, 720)],
+        format='JPEG',
+        options={'quality': 90},
+        null=True,
+        blank=True,
+    )
+    role = models.CharField(max_length=30, choices=ROLE_CHOICES)
+    slug = models.SlugField(unique=True, max_length=255, null=True, blank=True)
+    
+    added_by = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True, related_name='added_users')
+
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    created_at = models.DateTimeField(default=timezone.now)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    objects = CustomUserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['name', 'phone_number']
+
+    def __str__(self):
+        return self.email
+
+    def save(self, *args, **kwargs):
+        # Check if the instance already exists in the database
+        if self.pk:
+            orig = User.objects.get(pk=self.pk)
+            if orig.name != self.name:
+                # Name has changed; update the slug
+                self.slug = self.generate_unique_slug()
+        else:
+            # New instance; generate slug if not provided
+            if not self.slug:
+                self.slug = self.generate_unique_slug()
+
+        super(User, self).save(*args, **kwargs)
+
+    def generate_unique_slug(self):
+        """
+        Generates a unique slug from the user's name.
+        If the slug already exists, appends a numerical suffix to make it unique.
+        """
+        base_slug = slugify(self.name)
+        slug = base_slug
+        counter = 1
+        while User.objects.filter(slug=slug).exclude(pk=self.pk).exists():
+            slug = f"{base_slug}-{counter}"
+            counter += 1
+        return slug
+
+    def get_full_name(self):
+        """
+        Returns the user's full name.
+        """
+        return self.name
+
+    def get_short_name(self):
+        """
+        Returns the user's short name.
+        """
+        return self.name.split()[0] if self.name else self.email
