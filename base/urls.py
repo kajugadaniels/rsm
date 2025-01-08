fae13db09842bba6ec709c3d6f4784fbb5756c29
KAJUGA Daniels <kajugadaniels@gmail.com>
2025-01-08 13:06:41 +0200

    Add dashboard route

---
+++ base/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from base.views import *
+from django.conf import settings
+from django.conf.urls.static import static
+
+app_name = 'base'
+
+urlpatterns = [
+    path('dashboard/', dashboard, name="dashboard"),
+]  + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
