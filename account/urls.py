3f8ffaa1f381daaa1f2973343e6e82897b3a1ad6
KAJUGA Daniels <kajugadaniels@gmail.com>
2025-01-08 12:27:05 +0200

    Add login, logout and user profile update routes

--- account/urls.py
+++ account/urls.py
@@ -6,5 +6,7 @@ from django.conf.urls.static import static
 app_name = 'auth'
 
 urlpatterns = [
-    # 
+    path('', userLogin, name="login"),
+    path('logout/', userLogout, name='logout'),
+    path('profile/', userProfile, name='userProfile'),
 ]  + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
