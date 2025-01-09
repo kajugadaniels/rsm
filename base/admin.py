b2110a34f40606c9024131a6b00d9690124993b2
KAJUGA Daniels <kajugadaniels@gmail.com>
2025-01-09 13:25:48 +0200

    Register Order model in admin.py

--- base/admin.py
+++ base/admin.py
@@ -113,3 +113,34 @@ class ClientAdmin(admin.ModelAdmin):
             writer.writerow([client.name, client.phone_number, client.destination, client.created_at, client.updated_at])
 
         self.message_user(request, _("Selected clients have been exported as CSV."))
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    """
+    Admin interface for Order model.
+    """
+    list_display = ('orderId', 'client', 'destination', 'grand_total', 'addedBy', 'updatedBy', 'created_at', 'updated_at')
+    search_fields = ('orderId', 'client__name', 'client__phone_number', 'destination')
+    list_filter = ('created_at', 'updated_at', 'client__destination')
+    readonly_fields = ('orderId', 'grand_total', 'created_at', 'updated_at')
+
+    fieldsets = (
+        (None, {
+            'fields': ('orderId', 'client', 'destination')
+        }),
+        (_('Additional Information'), {
+            'fields': ('addedBy', 'updatedBy')
+        }),
+        (_('Timestamps'), {
+            'fields': ('created_at', 'updated_at')
+        }),
+        (_('Grand Total'), {
+            'fields': ('grand_total',)
+        }),
+    )
+
+    def save_model(self, request, obj, form, change):
+        if not obj.addedBy:
+            obj.addedBy = request.user
+        obj.updatedBy = request.user
+        super().save_model(request, obj, form, change)
