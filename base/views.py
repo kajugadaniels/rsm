b464eeb690837d2bcb14d0ff05d7779a5c83266c
KAJUGA Daniels <kajugadaniels@gmail.com>
2025-01-09 11:12:27 +0200

    Add function for adding new role

--- base/views.py
+++ base/views.py
@@ -19,3 +19,27 @@ def getRoles(request):
         'roles': roles,
     }
     return render(request, 'pages/roles/index.html', context)
+
+@login_required
+@permission_required('account.add_role', raise_exception=True)
+def addRole(request):
+    """
+    Create a new Role instance.
+    """
+    if request.method == 'POST':
+        form = RoleForm(request.POST)
+        if form.is_valid():
+            role = form.save()
+            form.save_m2m()  # Save the many-to-many data for permissions
+            messages.success(request, _("The role '%(role)s' has been created successfully.") % {'role': role.name})
+            return redirect(reverse('auth:getRoles'))
+        else:
+            messages.error(request, _("Please correct the errors below and try again."))
+    else:
+        form = RoleForm()
+    
+    context = {
+        'form': form,
+        'title': _('Add New Role'),
+    }
+    return render(request, 'pages/roles/create.html', context)
