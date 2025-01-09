9b51cde95522648e5a17ee7cdc20f87cb6356c51
KAJUGA Daniels <kajugadaniels@gmail.com>
2025-01-09 11:08:24 +0200

    Add role routes

--- base/urls.py
+++ base/urls.py
@@ -7,4 +7,9 @@ app_name = 'base'
 
 urlpatterns = [
     path('dashboard/', dashboard, name="dashboard"),
+
+    path('roles/', getRoles, name="getRoles"),
+    path('role/add/', addRole, name="addRole"),
+    path('role/edit/<slug>/', editRole, name="editRole"),
+    path('role/delete/<slug>/', deleteRole, name="deleteRole"),
 ]  + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- base/views.py
+++ base/views.py
@@ -1,4 +1,12 @@
-from django.shortcuts import render
+from base.forms import *
+from account.models import *
+from django.urls import reverse
+from django.contrib import messages
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.decorators import login_required, user_passes_test
+
+def is_superuser(user):
+    return user.is_superuser
 
 def dashboard(request):
     return render(request, 'pages/dashboard.html')
