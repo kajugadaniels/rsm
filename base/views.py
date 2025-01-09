9991f0a2ec07feef350f5954b8e15f8c37d2e16c
KAJUGA Daniels <kajugadaniels@gmail.com>
2025-01-09 11:11:48 +0200

    Add function for getRoles

--- base/views.py
+++ base/views.py
@@ -2,11 +2,20 @@ from base.forms import *
 from account.models import *
 from django.urls import reverse
 from django.contrib import messages
-from django.shortcuts import render, redirect, get_object_or_404
-from django.contrib.auth.decorators import login_required, user_passes_test
-
-def is_superuser(user):
-    return user.is_superuser
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.decorators import permission_required, login_required
 
 def dashboard(request):
     return render(request, 'pages/dashboard.html')
+
+@login_required
+@permission_required('account.view_role', raise_exception=True)
+def getRoles(request):
+    """
+    Retrieve and display all Role instances.
+    """
+    roles = Role.objects.all().order_by('-created_at')
+    context = {
+        'roles': roles,
+    }
+    return render(request, 'pages/roles/index.html', context)
