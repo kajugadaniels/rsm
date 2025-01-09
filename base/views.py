511f5c4f36b45c912215feec4ea280e317880e6d
KAJUGA Daniels <kajugadaniels@gmail.com>
2025-01-09 14:00:02 +0200

    Update addClient view to handle AJAX requests and return JSON responses

--- base/views.py
+++ base/views.py
@@ -4,6 +4,9 @@ from account.models import *
 from django.urls import reverse
 from django.db import transaction
 from django.contrib import messages
+from django.http import JsonResponse
+from django.forms.models import model_to_dict
+from django.views.decorators.csrf import csrf_exempt
 from django.utils.translation import gettext_lazy as _
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import permission_required, login_required
@@ -203,20 +206,28 @@ def getClients(request):
 
 @login_required
 @permission_required('base.add_client', raise_exception=True)
+@csrf_exempt  # Only if you're not using CSRF tokens in AJAX; otherwise, remove this
 def addClient(request):
-    """
-    Create a new Client instance.
-    """
     if request.method == 'POST':
         form = ClientForm(request.POST)
         if form.is_valid():
             client = form.save()
+            if request.is_ajax():
+                data = {
+                    'id': client.id,
+                    'phone_number': client.phone_number,
+                    'name': client.name,
+                }
+                return JsonResponse({'success': True, 'client': data})
             messages.success(
                 request, 
                 _("The client '%(client)s' has been created successfully.") % {'client': client.name}
             )
             return redirect(reverse('base:getClients'))
         else:
+            if request.is_ajax():
+                errors = form.errors.as_json()
+                return JsonResponse({'success': False, 'errors': errors})
             messages.error(request, _("Please correct the errors below and try again."))
     else:
         form = ClientForm()
